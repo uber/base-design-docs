38c937e1c303d8bdca064cf9efcd8283df5381ea
Graham Murdoch <murdoch@uber.com>
2020-07-20 12:46:52 -0700

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,24 @@
 import { AppProps } from "next/app";
-
+import Router from "next/router";
+import { useEffect } from "react";
 import { Provider as StyletronProvider } from "styletron-react";
 import { BaseProvider, LightThemeMove } from "baseui";
-
 import { styletron, debug } from "../lib/styletron";
 import Layout from "../components/layout";
+import * as gtag from "../lib/gtag";
 
 import "../styles.css";
 
 function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    Router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
   return (
     <div>
       <StyletronProvider value={styletron} debug={debug} debugAfterHydration>
